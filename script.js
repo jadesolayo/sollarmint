5b3f12c508f7cc644a916b1f6e9cf23d307afae3
jadesolayo <oloyedeyetundejadesola@gmail.com>
2022-10-16 14:59:00 +0100

    created media queries.css and modified index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function leftToggler() {
+  console.log(document.getElementById("nav"));
+  document.getElementById("nav").style.left = "0%";
+}
+
+function closeToggler() {
+  console.log(document.getElementById("nav"));
+  document.getElementById("nav").style.left = "100%";
+}

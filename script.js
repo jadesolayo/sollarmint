ba7e3faf534b955e9907217e041002429ca1c1e4
jadesolayo <oloyedeyetundejadesola@gmail.com>
2022-11-07 19:23:10 +0100

    modified index.html and style.css

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function closeToggler() {
   console.log(document.getElementById("nav"));
   document.getElementById("nav").style.left = "100%";
 }
+
+const d = document.getElementById("harmburger");
+const show = document.querySelector(".explore-btn-cont-hid");
+d.addEventListener("click", () => {
+  show.classList.toggle("show")
+});
